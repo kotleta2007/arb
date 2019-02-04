02a9c1c4cc1c9211c93ea19a3b2fe2393186db7b
Mark Tropin <mark@localhost.localdomain>
2019-02-04 22:52:03 +0300

    Added task

--- src/arb.cpp
+++ src/arb.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <string>
 
+// Add subtraction (Common Core) and regular subtraction
+
 // Add case if one of numbers is zero
 
 // Add MSB comparison in multString and powerString
